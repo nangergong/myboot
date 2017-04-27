d5344cb8b070ce6544151876400028896de0b935
nanger <nangergong@gmail.com>
2017-04-27 18:06:52 +0800

    最开始

---
+++ src/main/java/com/myboot/Application.java
@@ -0,0 +1,14 @@
+package com.myboot;
+
+/**
+ * Created by nanger on 27/04/17.
+ */
+
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
