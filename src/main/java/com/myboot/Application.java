bc4a44078c68e6a369347e1d0585fb8ceb1e9d3a
nanger <nangergong@gmail.com>
2017-04-28 12:08:20 +0800

    修改Application

--- src/main/java/com/myboot/Application.java
+++ src/main/java/com/myboot/Application.java
@@ -5,10 +5,11 @@ package com.myboot;
  */
 
 
-@SpringBootApplication
+@SpringBootApplication("nobody"")
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+
+        SpringApplication.run(Application.class, arg);
     }
 }
