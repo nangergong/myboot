189182c9c51db6bb9492b81bd711648034930b37
nanger <nangergong@gmail.com>
2017-04-28 12:44:30 +0800

    修改注解

--- src/main/java/com/myboot/Application.java
+++ src/main/java/com/myboot/Application.java
@@ -5,7 +5,7 @@ package com.myboot;
  */
 
 
-@SpringBootApplication("nobody"")
+@SpringBootApplication("modify annotation")
 public class Application {
 
     public static void main(String[] args) {
